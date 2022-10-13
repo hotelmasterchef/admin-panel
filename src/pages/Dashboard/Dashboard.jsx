e2061decc8ea67637f3448db682e242871d73d4d
sahoochinmay <sahoochinmay54@gmail.com>
2022-10-13 09:45:06 +0530

    Dashboard basic setup donee

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const navigate = useNavigate();
-  const { isLoggedIn } = useGlobalContext();
+  const { isLoggedIn, foods, menus } = useGlobalContext();
   useEffect(() => {
     if (!isLoggedIn) {
       navigate("/admin-panel/");
@@ -28,65 +28,47 @@ const Dashboard = () => {
         </ul>
       </div>
       <div className="row">
-        <div className="col-md-6 col-lg-3">
+        <div className="col-md-6 col-lg-6">
           <div className="widget-small primary coloured-icon">
-            <i className="icon fa fa-users fa-3x"></i>
+            <i className="icon fa fa-bandcamp fa-3x"></i>
             <div className="info">
-              <h4>Users</h4>
+              <h4>New Orders</h4>
               <p>
-                <b>5</b>
+                <b>0</b>
               </p>
             </div>
           </div>
         </div>
-        <div className="col-md-6 col-lg-3">
+        <div className="col-md-6 col-lg-6">
           <div className="widget-small info coloured-icon">
             <i className="icon fa fa-thumbs-o-up fa-3x"></i>
             <div className="info">
-              <h4>Likes</h4>
+              <h4>Total Orders</h4>
               <p>
-                <b>25</b>
+                <b>0</b>
               </p>
             </div>
           </div>
         </div>
-        <div className="col-md-6 col-lg-3">
+        <div className="col-md-6 col-lg-6">
           <div className="widget-small warning coloured-icon">
             <i className="icon fa fa-files-o fa-3x"></i>
             <div className="info">
-              <h4>Uploades</h4>
+              <h4>Total Menus</h4>
               <p>
-                <b>10</b>
+                <b>{menus?.length}</b>
               </p>
             </div>
           </div>
         </div>
-        <div className="col-md-6 col-lg-3">
+        <div className="col-md-6 col-lg-6">
           <div className="widget-small danger coloured-icon">
             <i className="icon fa fa-star fa-3x"></i>
             <div className="info">
-              <h4>Stars</h4>
+              <h4>Total Foods</h4>
               <p>
-                <b>500</b>
+                <b>{foods?.length}</b>
               </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="tile">
-            <h3 className="tile-title">Monthly Sales</h3>
-            <div className="embed-responsive embed-responsive-16by9">
-              <canvas className="embed-responsive-item" id="lineChartDemo"></canvas>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="tile">
-            <h3 className="tile-title">Support Requests</h3>
-            <div className="embed-responsive embed-responsive-16by9">
-              <canvas className="embed-responsive-item" id="pieChartDemo"></canvas>
             </div>
           </div>
         </div>
